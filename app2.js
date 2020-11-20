cf7264a0de16377449dbaafb10c7ec9de0aaae06
Anne-Bé <anne.bgesset@gmail.com>
2020-11-20 14:36:21 +0100

    Mise à jour javascript

--- app2.js
+++ app2.js
@@ -1,33 +1,25 @@
 (function(){
     var questionContainers = Array.from(document.getElementsByClassName("question_container"));
-    console.log(questionContainers);
-
-    var closeAll = function(){
-        var textQuestions = Array.from(document.getElementsByClassName("text_question"));
-        var arrows= Array.from(document.getElementsByClassName("arrow_logo"));
-        var responses= Array.from(document.getElementsByClassName("response"));
-
-        textQuestions.forEach(function (textQuestion){
-            textQuestion.classList.remove("question_active");
-        });
-        arrows.forEach(function(arrow){
-            arrow.classList.remove("arrow_up");
-        });
-        responses.forEach(function(response){
-            response.classList.add("hidden");
-        });
-    }
 
     questionContainers.forEach(function (questionContainer){
         questionContainer.addEventListener("click", function(event){
             var textQuestion = questionContainer.querySelector (".text_question");
             var arrow = questionContainer.querySelector(".arrow_logo");
             var response = questionContainer.querySelector(".response");
-            closeAll(textQuestion.value);
 
             textQuestion.classList.toggle ("question_active");
             arrow.classList.toggle("arrow_up");
             response.classList.toggle("hidden");
+
+            questionContainers.forEach(function(questionContainerActive){
+                var textQuestionActive = questionContainerActive.querySelector (".text_question")
+                if (textQuestionActive.innerHTML!==textQuestion.innerHTML){
+                    questionContainerActive.querySelector(".arrow_logo").classList.remove("arrow_up");
+                    questionContainerActive.querySelector(".response").classList.add("hidden");
+                    questionContainerActive.querySelector(".text_question").classList.remove("question_active");
+                }
+
+            })
         })
     })
 })()
